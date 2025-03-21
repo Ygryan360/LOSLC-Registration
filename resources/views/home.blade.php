3de78e39cf82e4d1a1f67edaf84d27d364434cd2
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-03-21 21:44:16 +0000

    feat(public): add favicon and web app manifest for improved branding and mobile support

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Linux & Open Source Lovers Community | Inscription au tournoi</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 
 <body>
@@ -101,7 +105,8 @@
                                     Sélectionnez une option</option>
                                 <option value="Dev" {{ old('tournament_type') === 'Dev' ? 'selected' : '' }}>
                                     Développement</option>
-                                <option value="CyberSec" {{ old('tournament_type') === 'CyberSec' ? 'selected' : '' }}>
+                                <option value="CyberSec"
+                                    {{ old('tournament_type') === 'CyberSec' ? 'selected' : '' }}>
                                     Cybersécurité</option>
                             </select>
                             @error('tournament_type')
