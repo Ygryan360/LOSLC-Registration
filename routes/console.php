07a992a644413307a938feac003d67112021b523
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-03-25 19:13:17 +0000

    feat: add command to send confirmation emails to unconfirmed registrations

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Models\Registration;
+use App\Mail\ConfirmRegistration;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('send-confirmation-mail', function () {
+    $registrations = Registration::where('confirmed', false)->get();
+    foreach ($registrations as $registration) {
+        Mail::to($registration->email)->send(new ConfirmRegistration($registration));
+        $registration->confirmed = true;
+        $registration->save();
+    }
+    $this->comment('Confirmation emails sent to unconfirmed registrations');
+})->purpose('Send confirmation email to unconfirmed registrations');
