40a5b65c7711c2aeca62a9292ccb6a42a7b72766
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-04-07 16:23:17 +0000

    a

--- routes/console.php
+++ routes/console.php
@@ -11,11 +11,11 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('send-confirmation-mail', function () {
-    $registrations = Registration::where('confirmed', false)->get();
+    $registrations = Registration::all();
     foreach ($registrations as $registration) {
         Mail::to($registration->email)->send(new ConfirmRegistration($registration));
-        $registration->confirmed = true;
-        $registration->save();
+        // $registration->confirmed = true;
+        // $registration->save();
     }
     $this->comment('Confirmation emails sent to unconfirmed registrations');
 })->purpose('Send confirmation email to unconfirmed registrations');
